99d1bc9981dd7285d2441ac7013ced26efe31983
bkstud <bkstud@em.com>
2023-04-26 14:12:17 +0200

    add handle function for json exception and fix bug related to it

---
+++ app/router/call.py
@@ -0,0 +1,80 @@
+from typing import Callable, Optional, Union
+
+from fastapi import Depends
+from pydantic import create_model
+
+
+def create_get_call(in_: Union[dict, list],
+                    call_name: Optional[str] = None) -> Callable:
+    """Generator of GET handle functions returning dictionary data
+
+    Args:
+        in_: dictionary or list containing json data to return by call
+        call_name: name of the handle method to give to callable
+              and dynamically created query model
+    Returns:
+        callable: fastapi compatible
+    """
+
+    # return in case in_ is not indexble array
+    async def non_queryable_api_data():
+        "In case in_ is non indexable."
+        return in_
+
+    in_dict_schema = None
+    out_fn = None
+
+    if type(in_) is dict:
+        out_fn = non_queryable_api_data
+    elif type(in_) is list and type(in_[0]) is dict:
+        in_dict_schema = in_[0]
+    else:
+        out_fn = non_queryable_api_data
+
+    if in_dict_schema:
+        query_params = {str(k): (Optional[type(v)], None)
+                        for k, v in in_dict_schema.items()}
+
+        query_model = create_model("Query", **query_params)
+
+        # return in case in_ a is list of dictonaries with some keys
+        async def querable_api_data(params: query_model = Depends()):
+            """In case in_ is list of dictionaries"""
+            set_params = params.dict(exclude_none=True)
+            # TODO: Consider if deep properties should be accessable using dots
+            return [el for el in in_
+                    if all([el.get(k) == v
+                            for k, v in set_params.items()])
+                    ]
+
+        out_fn = querable_api_data
+
+    if call_name:
+        out_fn.__name__ = call_name
+
+    return out_fn
+
+
+def json_exception_call(exception: Exception,
+                        file_name: str,
+                        call_name: Optional[str] = None):
+    """Generates GET handle function in case exception hapened during
+        json loading.
+
+    Args:
+        exception: An exception that hapened during json opening
+        name: name of endpoint / json file withouth exception.
+    Returns:
+        callable: fastapi compatible
+        name: name of the handle method to give to callable
+                and dynamically  created query model
+    """
+    async def _response():
+        return {
+                "detail": f"The file {file_name} is not valid json.",
+                "error": f"Json decode error: {str(exception)}."
+            }
+
+    if call_name:
+        _response.__name__ = call_name
+    return _response
--- app/router/dynamic.py
+++ app/router/dynamic.py
@@ -1,79 +1,16 @@
-from fastapi import APIRouter, Depends
-from typing import Union, Optional, Callable
-from pydantic import create_model
 import json
 import pathlib
-from app.common.json import stringify_text_entries_shallow
+
+from fastapi import APIRouter
+
 from app.common.file import get_files_by_type
+from app.common.json import stringify_text_entries_shallow
 from app.common.utils import logger
+
+from .call import create_get_call, json_exception_call
 
 
 class DynamicJsonRouter(APIRouter):
-
-    @staticmethod
-    def create_get_call(in_: Union[dict, list],
-                        name: Optional[str] = None) -> Callable:
-        """Generator of GET handle functions returning dictionary data
-
-        Args:
-            in_: dictionary or list containing json data to return by call
-        Returns:
-            callable: fastapi compatible
-            name: name of the handle method to give to callable
-                  and dynamically  created query model
-        """
-
-        # return in case in_ is not indexble array
-        async def non_queryable_api_data():
-            "In case in_ is non indexable."
-            return in_
-
-        in_dict_schema = None
-        out_fn = None
-
-        if type(in_) is dict:
-            # TODO: To be considered if in case of just dictionary
-            # we want to have fixed model for example key='somekey'
-            # that would make call return just single dictionary
-            # key
-            out_fn = non_queryable_api_data
-        elif type(in_) is list and type(in_[0]) is dict:
-            # TODO: Probably this is too simplified and we should
-            # check if each dictionary has the same keys or create set
-            # containing keys from all data.
-            # The edge case: input list [{}, {"foo": "bar", "name": "some"},
-            #  {...}]
-            in_dict_schema = in_[0]
-        else:
-            out_fn = non_queryable_api_data
-
-        if in_dict_schema:
-            query_params = {str(k): (Optional[type(v)], None)
-                            for k, v in in_dict_schema.items()}
-
-            query_model = create_model("Query", **query_params)
-
-            # return in case in_ a is list of dictonaries with some keys
-            async def querable_api_data(params: query_model = Depends()):
-                """In case in_ is list of dictionaries"""
-                set_params = params.dict(exclude_none=True)
-                # TODO: Consider if deep properties should be accessable using
-                return [el for el in in_
-                        if all([el.get(k) == v
-                               for k, v in set_params.items()])
-                        ]
-
-            out_fn = querable_api_data
-
-        if name:
-            out_fn.__name__ = name
-
-        return out_fn
-
-    @staticmethod
-    def handle_json_exception():
-        ""
-        raise NotImplementedError()
 
     def __init__(self, input_directory: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -83,19 +20,23 @@ class DynamicJsonRouter(APIRouter):
         relative = [f.relative_to(base).with_suffix("") for f in share_files]
 
         for file_, api_path in zip(share_files, relative):
+            call_name = f"Get {api_path.name}"
+
             with open(file_, "r") as infile:
                 try:
                     json_ = json.load(infile)
                 except json.decoder.JSONDecodeError as exc:
-                    # TODO: Make this exception more verbose
-                    # but service should start even if there is broken json
-                    # TODO: Create special endpoint handling this situastopm
-                    # 50.. something code + json data telling what happened
-                    # wrong
-                    # {detail: "info about error from json file"}
-                    logger.error(exc)
+                    logger.warning(exc)
+                    self.add_api_route(f"/{api_path}",
+                                       json_exception_call(
+                                                         exc,
+                                                         file_name=file_,
+                                                         call_name=call_name),
+                                       methods=["GET"],
+                                       status_code=500)
+                    continue
+
             stringify_text_entries_shallow(json_)
-            call_name = f"Get {api_path.name}"
             self.add_api_route(f"/{api_path}",
-                               self.create_get_call(json_, name=call_name),
+                               create_get_call(json_, call_name=call_name),
                                methods=["GET"])
